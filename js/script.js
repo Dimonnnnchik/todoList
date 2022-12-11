86732f9e1bbe0bb8049ebdaa49403308a519e66f
Dimonnnnchik <ananaskorea96@gmail.com>
2022-12-11 03:48:01 +0100

    dima gotovo

--- js/script.js
+++ js/script.js
@@ -1,17 +1,44 @@
+let checked = document.querySelectorAll('.todo');
+let toDoText = document.querySelectorAll('.todo-text');
+let todoslist = document.querySelector('.todolist');
+let searchInput = document.getElementById('search');
 
-let checked = document.getElementById('todo');
-let toDoText = document.querySelector('#todo-text');
 
-checked.addEventListener('click', lineThrough)
+const filterList = (searchValue) => {
+    let lists = todoslist.querySelectorAll('li');
+    lists.forEach(list => {
+        let listText = list.innerText.toLowerCase();
+        if (listText.indexOf(searchValue) != -1) {
+            list.classList.remove('filtered');
+        } else {
+            list.classList.add('filtered');
+            let filteredList = todoslist.querySelectorAll('.filtered')
+        }
+    })
+}
+searchInput.addEventListener('keyup', e => {
+    let searchText = e.target.value.trim().toLowerCase();
+    filterList(searchText);
+})
+
+
+checked.forEach((element) => {
+    element.addEventListener('click', lineThrough)
+})
+
 
 
 function lineThrough() {
-    if (!checked.checked) {
-        toDoText.style.textDecoration = '';
+    for (let i = 0; i < checked.length; i++) {
+        if (!checked[i].checked) {
+            toDoText[i].style.textDecoration = '';
 
 
-    } else {
-        toDoText.style.textDecoration = 'line-through';
 
+        } else {
+            toDoText[i].style.textDecoration = 'line-through';
+
+        }
     }
+
 }
